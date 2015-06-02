b6167b141b0ae13ef034d63ef827a3e87f593653
Anton Dimitrov <dimitrov.anton@gmail.com>
2015-06-02 11:25:34 +0300

    Modifies socket name for unicorn.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.tasks_grader.sock"
+listen "/tmp/unicorn.codemarathon_grader.sock"
 worker_processes 2
 timeout 30
