8d12c9ee07c0eef1c6cfbcc002bd78bc0512a46e
Anton Dimitrov <dimitrov.anton@gmail.com>
2015-06-03 11:27:30 +0300

    Updates schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -9,63 +9,63 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150528134222) do
+ActiveRecord::Schema.define(version: 20150528134222) do
 
-  create_table "api_keys", :force => true do |t|
+  create_table "api_keys", force: true do |t|
     t.integer  "user_id"
     t.string   "access_token"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  create_table "runs", :force => true do |t|
+  create_table "runs", force: true do |t|
     t.integer  "user_id"
     t.integer  "task_id"
-    t.string   "status",        :limit => 32
-    t.datetime "created_at",                    :null => false
-    t.datetime "updated_at",                    :null => false
+    t.string   "status",        limit: 32
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "code"
-    t.string   "message",       :limit => 2048
+    t.string   "message",       limit: 2048
     t.text     "data"
     t.text     "log"
     t.integer  "max_memory_kb"
     t.integer  "max_time_ms"
   end
 
-  create_table "tasks", :force => true do |t|
+  create_table "tasks", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
     t.string   "checker"
-    t.string   "task_type",    :default => "iofiles"
+    t.string   "task_type",    default: "iofiles"
     t.text     "wrapper_code"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                                  :default => "", :null => false
-    t.string   "encrypted_password",                     :default => "", :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                               default: "", null: false
+    t.string   "encrypted_password",                  default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                          :default => 0,  :null => false
+    t.integer  "sign_in_count",                       default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                             :null => false
-    t.datetime "updated_at",                                             :null => false
-    t.string   "file_path",              :limit => 2048
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+    t.string   "file_path",              limit: 2048
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
   end
 
-  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
