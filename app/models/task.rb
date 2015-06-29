33592aae5c2c1066355bf0a789ef9489e6da9d33
Anton Dimitrov <dimitrov.anton@gmail.com>
2015-06-29 16:26:18 +0300

    Adds validation for the type of task created.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,14 @@ class Task < ActiveRecord::Base
   module Constants
     TASK_TYPE_IOFILES = "iofiles"
     TASK_TYPE_PYUNIT = "pyunit"
+
+    TASK_TYPES = [TASK_TYPE_IOFILES, TASK_TYPE_PYUNIT]
   end
+
+  include Constants
 
   has_many :runs, dependent: :destroy
   belongs_to :user
 
-  include Constants
+  validates :task_type, inclusion: { in: TASK_TYPES }
 end
